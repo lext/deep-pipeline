e7015ad74463565bbe1dd34810fc91a5d3efc862
lext <aleksei.tiulpin@protonmail.ch>
2019-05-28 07:13:07 +0300

    Default training loop added

--- deeppipeline/common/core.py
+++ deeppipeline/common/core.py
@@ -8,6 +8,8 @@ from torch import optim
 import os
 import operator
 from deeppipeline.kvs import GlobalKVS
+from tensorboardX import SummaryWriter
+from torch.optim import lr_scheduler
 
 
 # Return the git revision as a string
@@ -81,6 +83,29 @@ def init_optimizer(net):
         raise NotImplementedError
 
 
+def log_metrics(writer, train_loss, val_loss, val_results, val_results_callback=None):
+    kvs = GlobalKVS()
+
+    print(colored('==> ', 'green') + 'Metrics:')
+    print(colored('====> ', 'green') + 'Train loss:', train_loss)
+    print(colored('====> ', 'green') + 'Val loss:', val_loss)
+
+    to_log = {'train_loss': train_loss, 'val_loss': val_loss}
+    val_metrics = {'epoch': kvs['cur_epoch']}
+    val_metrics.update(to_log)
+    writer.add_scalars(f"Losses_{kvs['args'].annotations}", to_log, kvs['cur_epoch'])
+    if val_results_callback is not None:
+        val_results_callback(writer, val_metrics, to_log, val_results)
+
+    kvs.update(f'losses_fold_[{kvs["cur_fold"]}]', to_log)
+    kvs.update(f'val_metrics_fold_[{kvs["cur_fold"]}]', val_metrics)
+
+
+def init_ms_scheduler(optimizer):
+    kvs = GlobalKVS()
+    return lr_scheduler.MultiStepLR(optimizer, kvs['args'].lr_drop)
+
+
 def save_checkpoint(net, optimizer, val_metric_name, comparator='lt'):
     if isinstance(net, torch.nn.DataParallel):
         net = net.module
@@ -107,3 +132,54 @@ def save_checkpoint(net, optimizer, val_metric_name, comparator='lt'):
             kvs.update('prev_model', cur_snapshot_name)
             kvs.update('best_val_metric', val_metric)
 
+
+def train_fold(pass_epoch, net, train_loader, optimizer, criterion, val_loader, scheduler, log_metrics_cb=None):
+    kvs = GlobalKVS()
+    fold_id = kvs['cur_fold']
+    writer = SummaryWriter(os.path.join(kvs['args'].workdir, 'snapshots', kvs['snapshot_name'],
+                                        'logs', 'fold_{}'.format(fold_id), kvs['snapshot_name']))
+
+    for epoch in range(kvs['args'].n_epochs):
+        print(colored('==> ', 'green') + f'Training epoch [{epoch}] with LR {scheduler.get_lr()}')
+        kvs.update('cur_epoch', epoch)
+        train_loss, _ = pass_epoch(net, train_loader, optimizer, criterion)
+        val_loss, val_results = pass_epoch(net, val_loader, None, criterion)
+        log_metrics(writer, train_loss, val_loss, val_results, log_metrics_cb)
+        save_checkpoint(net, optimizer, 'val_loss', 'lt')
+        scheduler.step()
+
+
+def train_n_folds(init_args, init_metadata, init_augs,
+                  init_data_processing,
+                  init_folds,
+                  init_loaders,
+                  init_model, init_loss,
+                  init_scheduler,
+                  img_group_id_colname=None, img_class_colname=None):
+
+    args = init_args()
+    kvs = init_session(args)[-1]
+    init_metadata()
+    assert 'metadata' in kvs
+
+    if init_augs is None:
+        raise NotImplementedError('Train augmentations are not defined !!!')
+    else:
+        init_augs()
+    assert 'train_trf' in kvs
+    init_data_processing()
+    init_folds(img_group_id_colname=img_group_id_colname, img_class_colname=img_class_colname)
+
+    for fold_id, x_train, x_val in kvs['cv_split']:
+        kvs.update('cur_fold', fold_id)
+        kvs.update('prev_model', None)
+
+        net = init_model()
+        optimizer = init_optimizer(net)
+        criterion = init_loss()
+        scheduler = init_scheduler(optimizer)
+        train_loader, val_loader = init_loaders(x_train, x_val)
+
+        train_fold(net=net, train_loader=train_loader,
+                   optimizer=optimizer, criterion=criterion,
+                   val_loader=val_loader, scheduler=scheduler)
