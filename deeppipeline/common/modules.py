0c194ded290659071976397131562f5f1323cc93
lext <aleksei.tiulpin@protonmail.ch>
2019-05-28 07:36:41 +0300

    Conv modules are now in `common`

---
+++ deeppipeline/common/modules.py
@@ -0,0 +1,49 @@
+from torch import nn
+
+
+def conv_block_nxn(ks, inp, out, activation='relu', normalization='BN'):
+    """3x3 ConvNet building block with different activations and normalizations support.
+
+    Aleksei Tiulpin, Unversity of Oulu, 2017 (c).
+
+    """
+    if normalization == 'BN':
+        norm_layer = nn.BatchNorm2d(out)
+    elif normalization == 'IN':
+        norm_layer = nn.InstanceNorm2d(out)
+    elif normalization is None:
+        norm_layer = nn.Sequential()
+    else:
+        raise NotImplementedError
+
+    if activation == 'relu':
+        return nn.Sequential(
+            nn.Conv2d(inp, out, kernel_size=ks, padding=1),
+            norm_layer,
+            nn.ReLU(inplace=True)
+        )
+
+    elif activation == 'selu':
+        return nn.Sequential(
+            nn.Conv2d(inp, out, kernel_size=ks, padding=1),
+            nn.SELU(inplace=True)
+        )
+
+    elif activation == 'elu':
+        return nn.Sequential(
+            nn.Conv2d(inp, out, kernel_size=ks, padding=1),
+            nn.ELU(1, inplace=True)
+        )
+    elif activation is None:
+        return nn.Sequential(
+            nn.Conv2d(inp, out, kernel_size=ks, padding=1),
+            norm_layer,
+        )
+
+
+def conv_block3x3(inp, out, activation='relu', normalization='BN'):
+    return conv_block_nxn(3, inp, out, activation, normalization)
+
+
+def conv_block1x1(inp, out, activation='relu', normalization='BN'):
+    return conv_block_nxn(3, inp, out, activation, normalization)
--- deeppipeline/segmentation/models/modules.py
+++ deeppipeline/segmentation/models/modules.py
@@ -2,42 +2,7 @@ import torch.nn as nn
 import torch
 import torch.nn.functional as F
 
-
-def conv_block3x3(inp, out, activation='relu', normalization='BN'):
-    """3x3 ConvNet building block with different activations and normalizations support.
-
-    Aleksei Tiulpin, Unversity of Oulu, 2017 (c).
-
-    """
-    if normalization == 'BN':
-        norm_layer = nn.BatchNorm2d(out)
-    elif normalization == 'IN':
-        norm_layer = nn.InstanceNorm2d(out)
-    elif normalization is None:
-        norm_layer = nn.Sequential()
-    else:
-        raise NotImplementedError
-
-    if activation == 'relu':
-        return nn.Sequential(
-            nn.Conv2d(inp, out, kernel_size=3, padding=1),
-            norm_layer,
-            nn.ReLU(inplace=True)
-        )
-
-    elif activation == 'selu':
-        return nn.Sequential(
-            nn.Conv2d(inp, out, kernel_size=3, padding=1),
-            nn.SELU(inplace=True)
-        )
-
-    elif activation == 'elu':
-        return nn.Sequential(
-            nn.Conv2d(inp, out, kernel_size=3, padding=1),
-            nn.ELU(1, inplace=True)
-        )
-    else:
-        raise ValueError
+from deeppipeline.common.modules import conv_block3x3
 
 
 class Encoder(nn.Module):
